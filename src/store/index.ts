99e8846dded88701fe711033afbd4295e35e423c
Jude Luis Baptista <46082058+judebaptista18@users.noreply.github.com>
2026-03-31 13:09:59 +0100

    Update store - add comment

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchReducer from './slices/searchSlice';
 
+// Configure the Redux store with search slice reducer
 export const store = configureStore({
   reducer: {
     search: searchReducer,
   },
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store.
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
